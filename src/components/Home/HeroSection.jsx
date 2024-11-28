ba0824c11c98c4987ee36d9e765539083d709207
VPhong <viphong123456789@gmail.com>
2024-11-29 00:58:46 +0700

    updated to herosection in homepage

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <div className="border-t border-gray-300">
-      <div className="flex">
+      <div className="flex flex-col lg:flex-row">
         {/* Sidebar Categories */}
-        <div className="w-1/4 bg-white p-4 border-r">
+        <div className="w-full lg:w-1/4 bg-white p-4 border-r hidden lg:block">
           <ul className="space-y-4">
             <li className="flex justify-between items-center text-black font-medium cursor-pointer hover:text-gray-500">
               Woman’s Fashion
@@ -47,10 +47,10 @@ const HeroSection = () => {
         </div>
 
         {/* Main Banner */}
-        <div className="w-3/4 relative bg-black">
-          <div className="relative w-full h-80 text-white flex items-center justify-between px-8">
+        <div className="w-full lg:w-3/4 relative bg-black">
+          <div className="relative w-full h-80 text-white flex flex-col lg:flex-row items-center justify-between px-8">
             {/* Text Section */}
-            <div>
+            <div className="text-center lg:text-left">
               <h3 className="text-2xl font-semibold">iPhone 14 Series</h3>
               <h1 className="text-4xl font-bold mt-2">Up to 10% off Voucher</h1>
               <button className="mt-4 px-6 py-2 text-sm font-medium bg-white text-black rounded-full hover:bg-gray-200">
@@ -62,7 +62,7 @@ const HeroSection = () => {
             <img
               src=""
               alt="iPhone 14"
-              className="h-80 w-auto"
+              className="h-40 lg:h-80 w-auto"
             />
           </div>
 
